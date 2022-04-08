73c6931e93cdf10fb739cd63958e94379369843b
I-Indorf <irene.indorf@hotmail.com>
2022-04-07 19:21:12 -0500

    added index

--- src/main/java/com/tts/deployment/MainController.java
+++ src/main/java/com/tts/deployment/MainController.java
@@ -1,13 +1,14 @@
 package com.tts.deployment;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
+    @Controller
     public class MainController {
         @RequestMapping("/") //http:localhost:8080/
         public String home() {
-            return "Look Ma, no localhost!";
+            return "index";
         }
     }
 
